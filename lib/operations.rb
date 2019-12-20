ad0d991c64f9a335ca3551ed281168b1b97e3fbe
Chloe Foronda <caforonda103@yahoo.com>
2019-12-20 17:22:33 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+  puts true
+  if speed < 40
+  elsif speed > 60
+  elsif 40 < speed > 60
+else speed == 60 && 40
 end
 
 
 
 def not_safe?(speed)
-	
+  puts true ? 40 < speed > 60 : 60, 40
 end
 	
 
